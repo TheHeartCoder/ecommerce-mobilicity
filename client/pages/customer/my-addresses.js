2737266b41f19f6f1eb861ff92c1716f8d055649
arindamTier5 <arindam.paul@tier5.in>
2022-01-11 11:27:08 +0530

    Address management For Customer V1.0.0

--- client/pages/customer/my-addresses.js
+++ client/pages/customer/my-addresses.js
@@ -1,198 +1,272 @@
 import { List, Button } from 'antd';
 import {
-  PlusSquareOutlined,
-  DeleteOutlined,
-  EditOutlined,
+	PlusSquareOutlined,
+	DeleteOutlined,
+	EditOutlined,
 } from '@ant-design/icons';
 import HeadText from '../../components/HeadText';
 const data = [
-  {
-    title: 'Mr. Arindam Paul 1 || 7001722370',
-  },
-  {
-    title: 'Mr. Arindam Paul 2 || 7001722370',
-  },
-  {
-    title: 'Mr. Arindam Paul 3 || 7001722370',
-  },
-  {
-    title: 'Mr. Arindam Paul 4 || 7001722370',
-  },
-  {
-    title: 'Mr. Arindam Paul 4 || 7001722370',
-  },
+	{
+		title: 'Mr. Arindam Paul 1 || 7001722370',
+	},
+	{
+		title: 'Mr. Arindam Paul 2 || 7001722370',
+	},
+	{
+		title: 'Mr. Arindam Paul 3 || 7001722370',
+	},
+	{
+		title: 'Mr. Arindam Paul 4 || 7001722370',
+	},
+	{
+		title: 'Mr. Arindam Paul 4 || 7001722370',
+	},
 ];
 
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { addAddress, getUserAddress } from '../../redux/actions/address';
+import {
+	addAddress,
+	deleteAddress,
+	getUserAddress,
+	updateAddress,
+} from '../../redux/actions/address';
 import toast from 'react-hot-toast';
 
 const initialState = {
-  fullName: '',
-  phoneNo: '',
-  pinNo: '',
-  locality: '',
-  landmark: '',
-  phoneNo2: '',
-  fullAddress: '',
-  state: '',
+	fullName: '',
+	phoneNo: '',
+	pinNo: '',
+	locality: '',
+	landmark: '',
+	phoneNo2: '',
+	fullAddress: '',
+	state: '',
 };
 
 const MyAddresses = () => {
-  const [addressInfo, setAddressInfo] = useState(initialState);
-  const [formOpen, setFormOpen] = useState(false);
-  const dispatch = useDispatch();
+	const [addressInfo, setAddressInfo] = useState(initialState);
+	const [currentId, setCurrentId] = useState('');
+	const [formOpen, setFormOpen] = useState(false);
+	const dispatch = useDispatch();
 
-  const addressData = useSelector((state) => state.addressData);
-  const { adds, loading, success } = addressData;
+	const addressData = useSelector((state) => state.addressData);
+	const { adds, loading, success } = addressData;
 
-  useEffect(() => {
-    dispatch(getUserAddress());
-  }, []);
+	useEffect(() => {
+		dispatch(getUserAddress());
+	}, []);
 
-  const handleSave = () => {
-    Object.keys(addressInfo).forEach(function (key) {
-      if (addressInfo[key] === '') {
-        toast.error('Please fill all the fields');
-        return;
-      }
-    });
-    dispatch(addAddress(addressInfo));
-  };
+	useEffect(() => {
+		if (success) {
+			setAddressInfo(initialState);
+			setFormOpen(false);
+			setCurrentId('');
+		}
+	}, [success]);
 
-  return (
-    <>
-      <HeadText
-        headText='Your Address'
-        subText='Manage your addresses from here'
-      />
-      <div className='container'>
-        <div className='card'>
-          <div className='row p-4'>
-            <div className='col'>
-              <Button
-                type='primary'
-                className='float-right'
-                icon={<PlusSquareOutlined />}
-                onClick={() => setFormOpen(true)}
-              >
-                Add New Address
-              </Button>
-            </div>
-          </div>
-          {formOpen && (
-            <div className='row p-4'>
-              <div className='col-md-4 p-2'>
-                <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Your full name'
-                />
-              </div>
-              <div className='col-md-4 p-2'>
-                <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Your phone no'
-                />
-              </div>
-              <div className='col-md-4 p-2'>
-                <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Your pin no'
-                />
-              </div>
-              <div className='col-md-4 p-2'>
-                <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Your locality'
-                />
-              </div>
-              <div className='col-md-4 p-2'>
-                <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Your landmark'
-                />
-              </div>
-              <div className='col-md-4 p-2'>
-                <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Alternative phone number(Optional)'
-                />
-              </div>
-              <div className='col-md-8 p-2'>
-                <textarea
-                  className='form-control'
-                  placeholder='Full address'
-                ></textarea>
-              </div>
-              <div className='col-md-4 p-2'>
-                <select className='form-control'>
-                  <option>Select Your State</option>
-                  <option>West Bengal</option>
-                  <option>Bihar</option>
-                  <option>Maharastra</option>
-                </select>
-              </div>
-              <div className='col-md-12 p-2'>
-                <button
-                  className='btn btn-primary float-right m-2'
-                  onClick={handleSave}
-                >
-                  Save
-                </button>
-                <button
-                  className='btn btn-secondary float-right m-2'
-                  onClick={() => {
-                    setAddressInfo(initialState);
-                    setFormOpen(false);
-                  }}
-                >
-                  Cancel
-                </button>
-              </div>
-            </div>
-          )}
+	const handleSave = () => {
+		Object.keys(addressInfo).forEach(function (key) {
+			if (addressInfo[key] === '') {
+				toast.error('Please fill all the fields');
+				return;
+			}
+		});
+		if (currentId) {
+			dispatch(updateAddress(currentId, addressInfo));
+		} else {
+			dispatch(addAddress(addressInfo));
+		}
+	};
 
-          <hr />
-          <div className='row p-4'>
-            <div className='col'>
-              <List
-                itemLayout='horizontal'
-                dataSource={adds}
-                renderItem={(item) => (
-                  <List.Item>
-                    <List.Item.Meta
-                      title={item.title}
-                      description='AC 166 , Ashoke villa, 2nd floor, Prafulla Kanan (N), Keshtopur, Kolkata, West Bengal - 700101'
-                    />
+	return (
+		<>
+			<HeadText
+				headText='Your Address'
+				subText='Manage your addresses from here'
+			/>
+			<div className='container'>
+				<div className='card'>
+					<div className='row p-4'>
+						<div className='col'>
+							<Button
+								type='primary'
+								className='float-right'
+								icon={<PlusSquareOutlined />}
+								onClick={() => {
+									setFormOpen(true);
+									setCurrentId('');
+								}}
+							>
+								Add New Address
+							</Button>
+						</div>
+					</div>
+					{formOpen && (
+						<div className='row p-4'>
+							<div className='col-md-4 p-2'>
+								<input
+									type='text'
+									className='form-control'
+									placeholder='Your full name'
+									value={addressInfo.fullName}
+									onChange={(e) =>
+										setAddressInfo({ ...addressInfo, fullName: e.target.value })
+									}
+								/>
+							</div>
+							<div className='col-md-4 p-2'>
+								<input
+									type='text'
+									className='form-control'
+									placeholder='Your phone no'
+									value={addressInfo.phoneNo}
+									onChange={(e) =>
+										setAddressInfo({ ...addressInfo, phoneNo: e.target.value })
+									}
+								/>
+							</div>
+							<div className='col-md-4 p-2'>
+								<input
+									type='text'
+									className='form-control'
+									placeholder='Your pin no'
+									value={addressInfo.pinNo}
+									onChange={(e) =>
+										setAddressInfo({ ...addressInfo, pinNo: e.target.value })
+									}
+								/>
+							</div>
+							<div className='col-md-4 p-2'>
+								<input
+									type='text'
+									className='form-control'
+									placeholder='Your locality'
+									value={addressInfo.locality}
+									onChange={(e) =>
+										setAddressInfo({ ...addressInfo, locality: e.target.value })
+									}
+								/>
+							</div>
+							<div className='col-md-4 p-2'>
+								<input
+									type='text'
+									className='form-control'
+									placeholder='Your landmark'
+									value={addressInfo.landmark}
+									onChange={(e) =>
+										setAddressInfo({ ...addressInfo, landmark: e.target.value })
+									}
+								/>
+							</div>
+							<div className='col-md-4 p-2'>
+								<input
+									type='text'
+									className='form-control'
+									placeholder='Alternative phone number(Optional)'
+									value={addressInfo.phoneNo2}
+									onChange={(e) =>
+										setAddressInfo({ ...addressInfo, phoneNo2: e.target.value })
+									}
+								/>
+							</div>
+							<div className='col-md-8 p-2'>
+								<textarea
+									className='form-control'
+									placeholder='Full address'
+									value={addressInfo.fullAddress}
+									onChange={(e) =>
+										setAddressInfo({
+											...addressInfo,
+											fullAddress: e.target.value,
+										})
+									}
+								></textarea>
+							</div>
+							<div className='col-md-4 p-2'>
+								<select
+									className='form-control'
+									value={addressInfo.state}
+									onChange={(e) =>
+										setAddressInfo({ ...addressInfo, state: e.target.value })
+									}
+								>
+									<option>Select Your State</option>
+									<option>West Bengal</option>
+									<option>Bihar</option>
+									<option>Maharastra</option>
+								</select>
+							</div>
+							<div className='col-md-12 p-2'>
+								<button
+									className='btn btn-primary float-right m-2'
+									onClick={handleSave}
+								>
+									Save
+								</button>
+								<button
+									className='btn btn-secondary float-right m-2'
+									onClick={() => {
+										setAddressInfo(initialState);
+										setFormOpen(false);
+									}}
+								>
+									Cancel
+								</button>
+							</div>
+						</div>
+					)}
 
-                    <button
-                      className='btn btn-outline-dark p-2 m-2'
-                      onClick={() => {}}
-                    >
-                      <EditOutlined />
-                    </button>
-                    <button
-                      className='btn btn-outline-danger p-2'
-                      onClick={() => {}}
-                    >
-                      <DeleteOutlined />
-                    </button>
-                  </List.Item>
-                )}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+					<hr />
+					<div className='row p-4'>
+						<div className='col'>
+							<List
+								itemLayout='horizontal'
+								dataSource={adds}
+								renderItem={(item) => (
+									<List.Item>
+										<List.Item.Meta
+											title={`${item.fullName} (${item.phoneNo})`}
+											description={
+												<>
+													<p>
+														{item.locality} , {item.landmark}, {item.pinNo}
+													</p>
+													<p>
+														{item.fullAddress}, {item.state}
+													</p>
+													<p>{item.phoneNo2}</p>
+												</>
+											}
+										/>
+
+										<button
+											className='btn btn-outline-dark p-2 m-2'
+											onClick={() => {
+												setCurrentId(item._id);
+												setFormOpen(true);
+												setAddressInfo({ ...initialState, ...item });
+											}}
+										>
+											<EditOutlined />
+										</button>
+										<button
+											className='btn btn-outline-danger p-2'
+											onClick={() => {
+												dispatch(deleteAddress(item._id));
+											}}
+										>
+											<DeleteOutlined />
+										</button>
+									</List.Item>
+								)}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default MyAddresses;
